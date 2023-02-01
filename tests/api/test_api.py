fdd5474c06afc210fee9160622d3264a5f892791
KateNosach19 <catnosah@ukr,net>
2023-02-02 00:49:37 +0200

    Add fixture and config

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,6 +1,15 @@
-def test_check_math():
-    assert 7 * 7 == 49
+import pytest
 
+@pytest.mark.change
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
 
-def test_chechmath_78():
-    assert 7 * 8 == 56
+@pytest.mark.check
+def test_name(user):
+    assert user.name == 'Kate'
+
+@pytest.mark.check
+
+def test_second_name(user):
+    assert user.second_name == 'Nosach'
