cdeb5de23f637af4ddb63786805624ec32c69238
KateNosach19 <catnosah@ukr,net>
2023-02-05 09:14:10 +0200

    Tests to requests

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -4,9 +4,25 @@ import requests
 @pytest.mark.http
 def test_first_request():
     r = requests.get('https://api.github.com/zen')
-    print(r.text)
+    print(f"Response is {r.text}")
 
 @pytest.mark.http
 def test_second_request():
-    r = requests.get('https://api.github.com/user/defunkt')
-    print(f"Response is {r.text}")    
+    r = requests.get('https://api.github.com/users/defunkt')
+    body = r.json()
+    headers = r.headers
+    
+    assert body['name'] == 'Chris Wanstrath'
+    assert body['blog'] == 'http://chriswanstrath.com/'
+    assert body['public_repos'] == 107
+    assert r.status_code == 200
+    assert headers['Server'] == 'GitHub.com'
+    assert headers['Cache-Control'] == 'public, max-age=60, s-maxage=60'
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('http://api.github.com/users/sergii_butenko')
+    
+    assert r.status_code == 404
+    assert r.status_code != 200 
+
